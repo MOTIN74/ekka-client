bfb7bbd543fe0c2ee9c55bb16d9e5a4e8c656135
KHAN <amotin68@gmail.com>
2022-01-03 14:40:37 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBD8ai-3m-48NFIhOdy3ApxrzwE6uGQ-Fg",
+  authDomain: "ekka-728a2.firebaseapp.com",
+  projectId: "ekka-728a2",
+  storageBucket: "ekka-728a2.appspot.com",
+  messagingSenderId: "215036778247",
+  appId: "1:215036778247:web:cead935d5de468689397a2",
+  measurementId: "G-X14ZNRS5VM"
+};
+export default firebaseConfig;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <br/>
+            
+            <h2>This is Banner</h2>
+            <img style={{ width: "250px" }} src="https://image.freepik.com/free-vector/isometric-data-protection-concept-with-parent-child-login-window-lock-3d_1284-63713.jpg" alt="" />
+        </div>
+    );
+};
+
+export default Banner;
